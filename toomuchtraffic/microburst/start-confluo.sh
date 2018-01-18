d512bf1b369722cc575ab822d7569770d6d8169e
Anurag Khandelwal <anuragk@berkeley.edu>
2018-01-18 12:52:21 -0800

    Remove unused files

--- toomuchtraffic/microburst/start-confluo.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-epid=${1:-"1"}
-confluo_path=${2:-"/data/anuragk/confluo"}
-
-port=$((9090 + $epid))
-nohup $confluo_path/build/bin/confluod --address=0.0.0.0 --port $port\
-    --data-path=$confluo_path/data/host_$i 2>$confluo_path/log/stderr/host_$i.stderr\
-    1>$confluo_path/log/stdout/host_$1.stdout &
