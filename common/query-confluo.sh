3ef6a104ce7bc9c909c61a79354065e0e0b9b451
Anurag Khandelwal <anuragk@berkeley.edu>
2018-01-19 11:21:38 -0800

    Fixed query-confluo script

--- common/query-confluo.sh
+++ common/query-confluo.sh
@@ -4,9 +4,10 @@ sbin="`dirname "$0"`"
 sbin="`cd "$sbin"; pwd`"
 
 ep_list=${1:-"eps.txt"}
-query=${2:-""}
+filter=${2:-""}
+aggregate=${3:-""}
 
 IFS=$'\r\n' GLOBIGNORE='*' command eval "EPS=($(cat $ep_list))"
 echo "Endpoints: ${EPS[@]}"
 
-$sbin/query-confluo/build/bin/query_confluo $ep_list $query
+$sbin/filter-confluo/build/bin/filter_confluo $ep_list $filter $aggregate
