c54b79e03f969507e6648b357ecf8dc881557a9e
Anurag Khandelwal <anuragk@berkeley.edu>
2018-01-19 11:22:33 -0800

    Remove redundant print info from query-confluo script

--- common/query-confluo.sh
+++ common/query-confluo.sh
@@ -7,7 +7,4 @@ ep_list=${1:-"eps.txt"}
 filter=${2:-""}
 aggregate=${3:-""}
 
-IFS=$'\r\n' GLOBIGNORE='*' command eval "EPS=($(cat $ep_list))"
-echo "Endpoints: ${EPS[@]}"
-
 $sbin/filter-confluo/build/bin/filter_confluo $ep_list $filter $aggregate
