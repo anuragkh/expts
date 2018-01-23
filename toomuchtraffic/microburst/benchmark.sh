afb78f8b8ecf79e742439a7a36b00b9b222f44f5
Anurag Khandelwal <anuragk@berkeley.edu>
2018-01-23 14:24:34 -0800

    Updated toomuchtraffic

--- toomuchtraffic/microburst/benchmark.sh
+++ toomuchtraffic/microburst/benchmark.sh
@@ -5,13 +5,13 @@ sbin="`cd "$sbin"; pwd`"
 
 host=${1:-"localhost"}
 
-function gen_eps() {
+function gen_queries() {
   host=$1
   nflows=$2
-  rm -f eps.txt
+  rm -f queries.txt
   for ep in `seq 2 $nflows`; do
     port=$((9090 + ep))
-    echo "$host:$port:flow${ep}" >> eps.txt
+    echo "$host:$port:flow${ep}:vlan1_tag==256&&vlan2_tag==36609:SUM(ipv4_total_length)" >> queries.txt
   done
 }
 
@@ -19,16 +19,16 @@ $sbin/../../common/teardown.sh $host
 
 for nudp in 1 2 4 8 16; do
   $sbin/../setup.sh microburst $host $nudp
-  gen_eps $host $((nudp + 1))
+  gen_queries $host $((nudp + 1))
   # Warmup runs
   for run in {1..10}; do
-    $sbin/../diagnosis.sh eps.txt vlan1_tag==256\&\&vlan2_tag==36609 SUM\(ipv4_total_length\)
+    $sbin/../diagnosis.sh queries.txt 
   done
   # Actual runs
   for run in {1..10}; do
-    $sbin/../diagnosis.sh eps.txt vlan1_tag==256\&\&vlan2_tag==36609 SUM\(ipv4_total_length\)
+    $sbin/../diagnosis.sh queries.txt
   done
   mv results.txt udp${nudp}.txt
-  rm -f eps.txt
+  rm -f queries.txt
   $sbin/../../common/teardown.sh $host
 done
