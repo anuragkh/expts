be7fde8d24baa139e8a14673db43efecba9b9505
Anurag Khandelwal <anuragk@berkeley.edu>
2018-01-23 14:22:43 -0800

    Fixes to toomanyredlights experiment

--- toomanyredlights/diagnosis.sh
+++ toomanyredlights/diagnosis.sh
@@ -3,4 +3,4 @@
 sbin="`dirname "$0"`"
 sbin="`cd "$sbin"; pwd`"
 
-$sbin/../common/query-confluo.sh $@ 1>>results.txt 2>>log.stderr
+$sbin/../common/query-confluo.sh $sbin/queries.txt 1>>results.txt 2>>log.stderr
